ac5ea5560bfd892025774bda8656cd74106166e7
gugdun <gugdunoff@gmail.com>
2024-12-05 16:31:15 +0300

    add: Scheduler base, spawner skins support #4

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -2,24 +2,19 @@ class_name Spawner
 
 extends Node3D
 
-@export var donut: PackedScene
-@export var timer: Timer
+@export var default_donuts: Array[PackedScene]
 @export var force: float = 225.0
 @export var angular_speed: float = 40.0
 @export var angular_velocity: float = 7.5
 
-func _ready() -> void:
-	timer.start()
-	
-func spawn() -> void:
-	if (GameState.current_state != GameState.State.PLAYING):
-		return
-	var inst: Donut = donut.instantiate()
+func _spawn_donut(donut: Donut) -> void:
 	var rand: Vector3 = Vector3(randf() * angular_speed * 2.0 - angular_speed, 0, 0)
-	inst.angular_velocity = Vector3(randf() * angular_velocity, randf() * angular_velocity, randf() * angular_velocity)
-	add_child(inst)
-	inst.set_spawner(self)
-	inst.apply_force(Vector3.UP * force + rand)
-
-func _on_hp_container_you_died():
-	timer.stop() 
+	donut.angular_velocity = Vector3(randf() * angular_velocity, randf() * angular_velocity, randf() * angular_velocity)
+	add_child(donut)
+	donut.global_position = global_position
+	donut.set_spawner(self)
+	donut.apply_force(Vector3.UP * force + rand)
+	
+func spawn_default_donut() -> void:
+	var d: Donut = default_donuts.pick_random().instantiate()
+	_spawn_donut(d)
